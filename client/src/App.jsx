9cd4d1b84e84964e0c6fc4390a8cec6e9fb4a2ee
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-05-11 11:13:28 +0530

    Implement Redux auth state management and route protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './Pages/Home'
 import Auth from './Pages/Auth'
 import { useEffect } from 'react';
-import { getCurrentUser } from './services/api';
+import { getCurrentUser } from './services/api.js';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const serverUrl = "http://localhost:8000";
 function App() {
 
+  const dispatch = useDispatch()
   useEffect(()=>{
-    getCurrentUser();
-  },[])
+    getCurrentUser(dispatch);
+  },[dispatch])
 
+  const {userData} = useSelector((state)=>state.user)
   return (
     <>
     <Routes>
-      <Route path ='/' element = {<Home/>} />
-      <Route path ='/auth' element = {<Auth/>} />
+      <Route path ='/' element = {userData?<Home/>:<Navigate to = "/auth" replace/>} />
+      <Route path ='/auth' element = {userData?<Navigate to = "/" replace/>:<Auth/>} />
     </Routes>
     </>
   )
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,15 @@
 import axios from "axios"
-import { serverUrl } from "../App"
+import { serverUrl } from "../App.jsx"
+import { setUserData } from "../redux/userSlice.js"
 
 
 
-export const getCurrentUser = async ()=>{
+export const getCurrentUser = async (dispatch)=>{
     try {
         const result = await axios.get(serverUrl + "/api/user/currentuser",{
             withCredentials: true
         })
-        console.log(result.data);
+        dispatch(setUserData(result.data));
     } catch (error) {
         console.log(error);
     }

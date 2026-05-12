581b9e1573792a52d00776d5bb016b13f569d66a
sainath tingilkar <tingilkarsainath0@gmail.com>
2026-05-12 13:01:38 +0530

    Complete homepage UI including navbar, hero section, and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import Auth from './Pages/Auth'
 import { useEffect } from 'react';
 import { getCurrentUser } from './services/api.js';
 import { useDispatch, useSelector } from 'react-redux';
+import History from './Pages/History.jsx';
+import Notes from './Pages/Notes.jsx';
+import Pricing from './Pages/Pricing.jsx';
 
 export const serverUrl = "http://localhost:8000";
 function App() {
@@ -20,6 +23,9 @@ function App() {
     <Routes>
       <Route path ='/' element = {userData?<Home/>:<Navigate to = "/auth" replace/>} />
       <Route path ='/auth' element = {userData?<Navigate to = "/" replace/>:<Auth/>} />
+      <Route path ='/history' element = {userData?<History/>:<Navigate to = "/auth" replace/>} />
+      <Route path ='/notes' element = {userData?<Notes/>:<Navigate to = "/auth" replace/>} />
+      <Route path ='/pricing' element = {userData?<Pricing/>:<Navigate to = "/auth" replace/>} />
     </Routes>
     </>
   )

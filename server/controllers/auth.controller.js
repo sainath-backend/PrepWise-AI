3e93be9a2dacff5a6c1b61b8ff78c506dbffb098
Sainath Tingilkar - dev <tingilkarsainath0@gmail.com>
2026-06-22 11:21:58 +0530

    Updated path for rewrite home page

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,8 @@ export const googleAuth = async (req,res)=>{
             httpOnly: true,
             secure : true,
             sameSite : "none",
-            maxAge: 7*24*60*60*1000
+            path: "/",
+            maxAge: 7*24*60*60*1000,
         })
         return res.status(200).json(user);
     } catch (error) {
